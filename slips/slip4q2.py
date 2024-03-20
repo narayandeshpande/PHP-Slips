58757ae1456d1380a1786fdb674c03311ccf9107
narayan pravin deshpande <102522755+narayandeshpande@users.noreply.github.com>
2024-03-20 21:10:48 +0530

    Delete slips directory

--- slips/slip4q2.py
+++
@@ -1,5 +0,0 @@
-#  Write a Python program to create a set with any 3 weekdays. Add single element to the set 
-# and print it. Add multiple elements and print the set. 
-days={'mon','fri','sat'}
-days.add('sum')
-print(days)
